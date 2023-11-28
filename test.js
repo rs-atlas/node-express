f589da5ab29eada73a106587f0e461dccbf6e89b
atlas <98959780+PotatoArtie@users.noreply.github.com>
2023-11-28 12:31:39 +0900

    [update] token key

--- test.js
+++ test.js
@@ -4,4 +4,4 @@ dotenv.config();
 import { GeekNewsRSS } from "./geeknews.js"
 import { sendMessage } from "./slack.js";
 
-sendMessage(process.env.GEEK_NEWS_BOT_TOKEN, "C067WTGCK9P", await GeekNewsRSS())
+sendMessage(process.env.DAILY_NEWS_BOT_TOKEN, "C067WTGCK9P", await GeekNewsRSS())
